1c778b2363addca0175c4f22a5a71c5d15dbeb75
PabloMenendez06 <pmenedez-2020332@kinal.edu.gt>
2024-08-27 11:45:39 -0600

    error solucionado

--- src/hooks/useGif.jsx
+++ src/hooks/useGif.jsx
@@ -3,7 +3,7 @@ export const useGif = () =>{
     const [gif, setGif] = useState('')
  
     const reqGif = async() =>{
-        await fetch('https://api.giphy.com/v1/gifs/random?api_key=Duo0cBkKLrMOIdJ90EaHE1rvBl8MdhNr').then(async(resp) =>{
+        await fetch('https://api.giphy.com/v1/gifs/random?api_key=NmNGrXFpvVDTMZZEIhThjA0mW7QymFIB').then(async(resp) =>{
             await resp.json().then(({data})=>{
                 setGif(data.images.original.url)
                 console.log(data)
